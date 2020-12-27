63141b59140e2dd0dec86f75cf44bf2b7d5c5e82
Kanisk <57884127+chakrakan@users.noreply.github.com>
2020-12-27 07:49:50 -0500

    metadata

--- gatsby-config.js
+++ gatsby-config.js
@@ -19,8 +19,8 @@ module.exports = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: `gatsby-starter-default`,
-        short_name: `starter`,
+        name: `discord-servercheck`,
+        short_name: `servercheck`,
         start_url: `/`,
         background_color: `#663399`,
         theme_color: `#663399`,
