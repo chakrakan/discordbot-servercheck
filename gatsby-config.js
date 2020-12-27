e888f940d14c6f688bb685ecc8e0754f7b09de73
Kanisk <57884127+chakrakan@users.noreply.github.com>
2020-12-27 07:49:10 -0500

    update config

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: `Discord Servercheck`,
+    description: `Provide your Bot account token to see which servers it's part of!`,
+    author: `@chakrakan`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
